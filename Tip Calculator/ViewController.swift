bc87b2ae7574ea8dd05abcb1fad4715d27c53c2b
Thomas Clifford <t.clifford@wustl.edu>
2016-01-01 01:33:01 -0500

    Added new features and improved UI

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipLabel: UILabel!
-    
+    @IBOutlet weak var total2: UILabel!
+    @IBOutlet weak var total3: UILabel!
+    @IBOutlet weak var total4: UILabel!
+    @IBOutlet weak var blackView: UIView!
     @IBOutlet weak var percentButton: UISegmentedControl!
     
     var key1 = "firstKey"
@@ -23,56 +26,34 @@ class ViewController: UIViewController {
     var percentages = [Double]()
     var openingTime: NSDate? = nil
     var closingTime: NSDate? = nil
-    
+    let formatter = NSNumberFormatter()
+    var tipLabelY: CGFloat = 0.0
 
-    func reset() {
-        billField.text=""
-        tipLabel.text = "$0.00"
-        totalLabel.text = "$0.00"
-        let defaults = NSUserDefaults.standardUserDefaults()
-        defaults.setObject("18", forKey: key1)
-        defaults.setObject("20", forKey: key2)
-        defaults.setObject("22", forKey: key3)
-        defaults.synchronize()
-        percentages.append(0.1)
-        percentages.append(0.1)
-        percentages.append(0.1)
-        percentButton.setTitle("18%", forSegmentAtIndex: 0)
-        percentButton.setTitle("20%", forSegmentAtIndex: 1)
-        percentButton.setTitle("22%", forSegmentAtIndex: 2)
-    }
-//    func compareTimes(opens: NSDate?, closes: NSDate?) {
-//        if(opens!.timeIntervalSinceReferenceDate-closes!.timeIntervalSinceReferenceDate>10) {
-//            reset()
-//        }
-//    }
-//    func openApp() {
-//        openingTime = NSDate()
-//        let defaults = NSUserDefaults.standardUserDefaults()
-//        closingTime = defaults.objectForKey(closeKey) as! NSDate?
-//        if (closingTime != nil) {
-//            print("did")
-//            compareTimes(openingTime, closes: closingTime)
-//        }
-//        print("opened")
-//    }
-//    
-//    func closeApp() {
-//        closingTime = NSDate()
-//        let defaults = NSUserDefaults.standardUserDefaults()
-//        defaults.setObject(closingTime, forKey: closeKey)
-//        print("closeed")
-//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tipLabelY = tipLabel.frame.origin.y
+        tipLabel.frame.origin.y += 400;
+        totalLabel.frame.origin.y += 400;
+        total2.frame.origin.y += 400;
+        total3.frame.origin.y += 400;
+        total4.frame.origin.y += 400;
+        blackView.frame.origin.y += 400;
+        percentButton.frame.origin.y += 400;
+        billField.frame.origin.y += 100;
+        
         // Do any additional setup after loading the view, typically from a nib.
+        formatter.numberStyle = .CurrencyStyle
+        billField.textAlignment = .Right
+        billField.attributedPlaceholder = NSAttributedString(string:formatter.currencySymbol,
+            attributes:[NSForegroundColorAttributeName: UIColor.blackColor()])
         reset()
+        
     }
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
 
-        
+        billField.becomeFirstResponder()
         let defaults = NSUserDefaults.standardUserDefaults()
         let first:Int? = Int(defaults.objectForKey(key1) as! String)
         let second:Int? = Int(defaults.objectForKey(key2) as! String)
@@ -88,14 +69,15 @@ class ViewController: UIViewController {
             percentButton.setTitle(String(third!)+"%", forSegmentAtIndex: 2)
         }
         else {
-            percentages[0] = 0.18
-            percentages[1] = 0.2
-            percentages[2] = 0.22
+            percentages[0] = 0.15
+            percentages[1] = 0.18
+            percentages[2] = 0.2
             
-            percentButton.setTitle("18%", forSegmentAtIndex: 0)
-            percentButton.setTitle("20%", forSegmentAtIndex: 1)
-            percentButton.setTitle("22%", forSegmentAtIndex: 2)
+            percentButton.setTitle("15%", forSegmentAtIndex: 0)
+            percentButton.setTitle("18%", forSegmentAtIndex: 1)
+            percentButton.setTitle("20%", forSegmentAtIndex: 2)
         }
+        
         let percent = percentages[percentButton.selectedSegmentIndex]
         
         if let billAmount = Double(billField.text!) {
@@ -103,11 +85,12 @@ class ViewController: UIViewController {
             let tip = billAmount*percent
             let total = billAmount + tip
             
-            tipLabel.text = String(format: "$%.2f", tip)
-            
-            totalLabel.text = String(format: "$%.2f", total)
+            tipLabel.text = "+ "+formatter.stringFromNumber(tip)!
+            totalLabel.text = formatter.stringFromNumber(total)
+            total2.text = formatter.stringFromNumber(total/2)
+            total3.text = formatter.stringFromNumber(total/3)
+            total4.text = formatter.stringFromNumber(total/4)
         }
-
     }
     
 
@@ -117,7 +100,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onEditingChanged(sender: AnyObject) {
-        
+
         let percent = percentages[percentButton.selectedSegmentIndex]
         
         if let billAmount = Double(billField.text!) {
@@ -125,15 +108,94 @@ class ViewController: UIViewController {
         let tip = billAmount*percent
         let total = billAmount + tip
         
-        tipLabel.text = String(format: "$%.2f", tip)
-
-        totalLabel.text = String(format: "$%.2f", total)
+        tipLabel.text = "+ "+formatter.stringFromNumber(tip)!
+        totalLabel.text = formatter.stringFromNumber(total)
+        total2.text = formatter.stringFromNumber(total/2)
+        total3.text = formatter.stringFromNumber(total/3)
+        total4.text = formatter.stringFromNumber(total/4)
+            if(tipLabelY != tipLabel.frame.origin.y) {
+            UIView.animateWithDuration(0.4, animations: {
+                self.tipLabel.frame.origin.y -= 400;
+                self.totalLabel.frame.origin.y -= 400;
+                self.total2.frame.origin.y -= 400;
+                self.total3.frame.origin.y -= 400;
+                self.total4.frame.origin.y -= 400;
+                self.blackView.frame.origin.y -= 400;
+                self.percentButton.frame.origin.y -= 400;
+                self.billField.frame.origin.y -= 100;
+            })
+            }
+        }
+         else if(billField.text!.isEmpty) {
+            if(tipLabelY == tipLabel.frame.origin.y) {
+            UIView.animateWithDuration(0.4, animations: {
+                self.tipLabel.frame.origin.y += 400;
+                self.totalLabel.frame.origin.y += 400;
+                self.total2.frame.origin.y += 400;
+                self.total3.frame.origin.y += 400;
+                self.total4.frame.origin.y += 400;
+                self.blackView.frame.origin.y += 400;
+                self.percentButton.frame.origin.y += 400;
+                self.billField.frame.origin.y += 100;
+            })
+            }
+            tipLabel.text = "+ "+formatter.stringFromNumber(0)!
+            totalLabel.text = formatter.stringFromNumber(0)
+            total2.text = formatter.stringFromNumber(0)
+            total3.text = formatter.stringFromNumber(0)
+            total4.text = formatter.stringFromNumber(0)
         }
     }
-  
+    
+    func reset() {
+        billField.text=""
+        tipLabel.text = "+ "+formatter.stringFromNumber(0)!
+        totalLabel.text = formatter.stringFromNumber(0)
+        total2.text = formatter.stringFromNumber(0)
+        total3.text = formatter.stringFromNumber(0)
+        total4.text = formatter.stringFromNumber(0)
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject("15", forKey: key1)
+        defaults.setObject("18", forKey: key2)
+        defaults.setObject("20", forKey: key3)
+        defaults.synchronize()
+        percentages.append(0.1)
+        percentages.append(0.1)
+        percentages.append(0.1)
+        percentButton.setTitle("15%", forSegmentAtIndex: 0)
+        percentButton.setTitle("18%", forSegmentAtIndex: 1)
+        percentButton.setTitle("20%", forSegmentAtIndex: 2)
+    }
+    
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
+    
+//        func compareTimes(opens: NSDate?, closes: NSDate?) {
+//            print("compare")
+//            if(opens!.timeIntervalSinceReferenceDate-closes!.timeIntervalSinceReferenceDate>3) {
+//                reset()
+//                print("op")
+//            }
+//        }
+//        func openApp() {
+//            print("open")
+//            openingTime = NSDate()
+//            let defaults = NSUserDefaults.standardUserDefaults()
+//            closingTime = defaults.objectForKey(closeKey) as! NSDate?
+//            if (closingTime != nil) {
+//                compareTimes(openingTime, closes: closingTime)
+//            }
+//        }
+//    
+//        func closeApp() {
+//            print("close")
+//            closingTime = NSDate()
+//            let defaults = NSUserDefaults.standardUserDefaults()
+//            defaults.setObject(closingTime, forKey: closeKey)
+//        }
+
 
 }
 
